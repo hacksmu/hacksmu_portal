e23b12e8f483cf544fc59c3b101a040bb0924ac5
myusernamesthisreal <myusernamesthis@gmail.com>
2022-07-17 11:23:21 -0500

    feat: Add robot pictures to FAQ

--- components/faq.tsx
+++ components/faq.tsx
@@ -6,6 +6,7 @@ import AboutHeader from './AboutHeader';
 import FaqDisclosure from './FaqDisclosure';
 import FaqDisplay from './FaqDisplay';
 import { RequestHelper } from '../lib/request-helper';
+import NextImage from 'next/image';
 
 /**
  * The FAQ page.
@@ -43,9 +44,28 @@ export default function FaqPage({ fetchedFaqs }: { fetchedFaqs: FAQ[] }) {
   }
 
   return (
-    <div className="z-0 relative bg-cool-white min-h-[100vh] pt-[5rem] xl:pt-[10rem]">
-      <div className="flex flex-col w-[calc(100vw + 2px)] xl:w-[90vw] pt-8 xl:pt-0 pb-24 xl:pb-64 px-0 relative container justify-center items-end">
-        <div className="rounded-[3rem] bg-light-red w-[90%] xl:w-[100%] relative z-10 mx-auto left-[-.5rem]">
+    <div className="z-0 relative bg-cool-white min-h-[100vh] pt-[5rem] xl:pt-[6rem]">
+      <div className="flex flex-col w-[calc(100vw + 2px)] xl:w-[85vw] pt-8 xl:pt-0 pb-24 xl:pb-64 px-0 relative container">
+        <div className="flex flex-row w-full items-center">
+          <div className="hidden xl:block relative self-start top-[5.5rem] left-[-6.5rem] mr-2">
+            <NextImage
+              className="transform-gpu rotate-[-30deg] scale-x-[-1]"
+              src="/assets/Charlotte.svg"
+              width={192}
+              height={192}
+            />
+          </div>
+          <div className="flex-1" />
+          <div className="hidden xl:block relative self-end top-[5.5rem] right-[-6.5rem] mr-2">
+            <NextImage
+              className="transform-gpu rotate-[30deg]"
+              src="/assets/Charlotte.svg"
+              width={192}
+              height={192}
+            />
+          </div>
+        </div>
+        <div className="rounded-[3rem] bg-light-red w-[90%] xl:w-[100%] relative z-10 mx-auto left-[-1.5rem]">
           <div className="rounded-[3rem] bg-white w-[100%] z-20 relative left-4 xl:left-10 top-3 pb-8 pt-1 px-2">
             <h1 className="text-4xl text-left tracking-widest mt-[2rem] pl-[2rem] text-dark-red font-bold">
               FAQ
