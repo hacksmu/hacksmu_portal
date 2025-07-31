42607e007298658d2ea9bee0fb91bf710afa1800
Luke Voinov <lukevoinov4000@gmail.com>
2025-07-31 13:05:45 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ import Link from 'next/link';
 import GradientDivider from '../components/GradientDivider';
 
 // Add a mock list of hackathons for the new section
+
 const moreHackathons = [
   {
     name: "CodeRED Astra",
@@ -48,7 +49,7 @@ export default function Home(props: {
         <meta name="description" content="HackSMU Portal" />
         <link rel="icon" href="/favicon2.ico" />
       </Head>
-     // section className="bg-contain bg-hero-pattern"
+      {/*<section className="bg-contain bg-hero-pattern"? */}
       <section className="relative h-screen overflow-hidden">
         <div className="hero-content">
           <h1 className="glow-text neon-title">HackSMU VII</h1>
