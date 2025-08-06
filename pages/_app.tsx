8cb28b40d4be0293490042bdca54117f170ccbe8
Luke Voinov <lukevoinov4000@gmail.com>
2025-08-06 16:46:32 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -60,7 +60,7 @@ function PortalApp({ Component, pageProps }: AppProps) {
               <meta name="theme-color" content="#5D5FEF" />
             </Head>
             <div className="min-h-screen flex flex-col bg-white mt-5">
-              {/* <AppHeader /> */}
+              <AppHeader />
               <Component {...pageProps} />
             </div>
           </FCMProvider>
