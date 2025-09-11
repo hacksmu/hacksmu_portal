ab9654e9c635026f9ce206f123ba471804d6c2bf
Luke Voinov <lukevoinov4000@gmail.com>
2025-09-11 09:42:10 -0500

    Fix FaqPage prop to use answered questions

--- pages/index.tsx
+++ pages/index.tsx
@@ -534,7 +534,7 @@ export default function Home(props: HomeProps) {
                     )}
 
                     {(faqs.length > 0 || (!faqLoading && !faqError)) && (
-                      <FaqPage fetchedFaqs={faqs} answeredQuestion={faqAnswered} />
+                      <FaqPage fetchedFaqs={faqAnswered} answeredQuestion={faqAnswered} />
                     )}
                   </div>
                 </div>
