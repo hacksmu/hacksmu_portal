64d6f0926a6b00d704875a15602f57af38b180fb
htameez <htameez@smu.edu>
2026-02-28 21:16:58 -0600

    Update background color for EventDetailLink and global styles to #000033 for consistency

--- components/EventDetailLink.tsx
+++ components/EventDetailLink.tsx
@@ -8,12 +8,12 @@ export default function EventDetailLink({ title }: EventDetailLinkProps) {
   return (
     <div
       className="border-b-2 py-2 flex flex-row items-center gap-x-2"
-      style={{ borderColor: '#7B81FF' }}
+      style={{ borderColor: '#000033' }}
     >
-      <h1 className="text-lg font-bold" style={{ color: '#7B81FF' }}>
+      <h1 className="text-lg font-bold" style={{ color: '#000033' }}>
         {title}
       </h1>
-      <ArrowNarrowRightIcon className="w-5 h-5" style={{ color: '#7B81FF' }} />
+      <ArrowNarrowRightIcon className="w-5 h-5" style={{ color: '#000033' }} />
     </div>
   );
 }
