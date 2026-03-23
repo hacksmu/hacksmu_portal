e753b8c23eab53f123c458674eb825a5d01d3aaf
aag2104 <aag2104@tx.rr.com>
2026-03-23 11:57:46 -0500

    added more logos

--- pages/index.tsx
+++ pages/index.tsx
@@ -356,6 +356,8 @@ export default function Home({ answeredQuestion }: HomeProps) {
             <div className="powered-by-logos" style={{ display: 'flex', flexDirection: 'column', gap: 0, alignItems: 'center' }}>
               <img className="powered-by-logo-smu" src="/sponsors/SMULyleLogo.png" alt="SMU Lyle School of Engineering" style={{ height: 450, width: 'auto', objectFit: 'contain' }} />
               <img className="powered-by-logo-imasons" src="/sponsors/iMasonsLogo.png" alt="iMason's" style={{ height: 450, width: 'auto', objectFit: 'contain', marginTop: -350 }} />
+              <img className="powered-by-logo-grundfos" src="/sponsors/grundfos.png" alt="Grundfos" style={{ height: 450, width: 'auto', objectFit: 'contain', marginTop: -320 }} />
+              <img className="powered-by-logo-purebuttons" src="/sponsors/pureButtons.png" alt="pureButtons" style={{ height: 150, width: 'auto', objectFit: 'contain', marginTop: -240, marginBottom: 30 }} />
             </div>
           </div>
         </div>
